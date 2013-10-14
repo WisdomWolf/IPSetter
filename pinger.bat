8ac7ccf3229da0663423788b4f87f0688f0851c7
WisdomWolf <WisdomWolf@gmail.com>
2013-10-14 12:57:30 -0400

    Added logging

--- pinger.bat
+++ pinger.bat
@@ -2,6 +2,8 @@
 
 IF NOT EXIST ipaddresslist.txt GOTO missinglist
 IF EXIST pingtemp.txt DEL pingtemp.txt
+ECHO %DATE% >> pinger_log.txt
+ECHO.>> pinger_log.txt
 
 
 setlocal EnableDelayedExpansion
@@ -9,6 +11,8 @@ setlocal EnableDelayedExpansion
 :pingloop
 FOR /F "tokens=1 delims=&&" %%A IN (ipaddresslist.txt) DO (
 	ping -n 1 %%A | FIND /I "reply" > pingtemp.txt
+	ECHO %%A >> pinger_log.txt
+	TYPE pingtemp.txt >> pinger_log.txt
 	FOR /F "tokens=6 delims= " %%B IN (pingtemp.txt) DO SET _response=%%B
 	DEL pingtemp.txt
 	CALL :available_check %%A !_response!
@@ -22,9 +26,15 @@ SET status=%2
 IF %status%==unreachable. (
 	ECHO %ip% is available.
 	CHOICE /C:YN /M "Would you like to use %ip%? "
-	IF ERRORLEVEL 2 GOTO :eof
-	IF ERRORLEVEL 1 CALL ipsetter.bat %ip%
-	REM CALL ipsetter.bat %ip%
+	IF ERRORLEVEL 2 (
+		ECHO User chose not to use %ip%. >> pinger_log.txt
+		GOTO :eof
+	)
+	IF ERRORLEVEL 1 (
+		ECHO Calling ipsetter using %ip%. >> pinger_log.txt
+		REM IF ERRORLEVEL 1 CALL ipsetter.bat %ip%
+		ECHO "ipsetter template".
+	)
 	GOTO endX
 ) ELSE (GOTO :eof)
 
@@ -36,7 +46,10 @@ GOTO endX
 
 :end
 ECHO No available IP found.
+ECHO Exited without selecting a valid IP.>>pinger_log.txt
 PAUSE
 
 :endX
+ECHO ------------------------------------------------------>>pinger_log.txt
+ECHO.>>pinger_log.txt
 EXIT
