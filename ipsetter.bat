e1b24074eabdb8534dfa9c987925eea0e7f1da04
WisdomWolf <WisdomWolf@gmail.com>
2013-10-14 10:52:42 -0400

    Updated Default Gateway

--- ipsetter.bat
+++ ipsetter.bat
@@ -2,7 +2,7 @@
 
 SET Net_Adapt="Local Area Connection"
 SET IP_Addr=%1
-SET D_Gate=
+SET D_Gate=128.3.1.83
 SET Sub_Mask=255.255.0.0
 ECHO "Setting Static IP Information" 
 netsh interface ip SET address %Net_Adapt% static %IP_Addr% %Sub_Mask% %D_Gate% 1 
