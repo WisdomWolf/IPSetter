7db6fc26ce1fbb0109f27d378e15fde9c7f93fe0
WisdomWolf <WisdomWolf@gmail.com>
2013-10-14 12:40:59 -0400

    Added DNS entries

--- ipsetter.bat
+++ ipsetter.bat
@@ -4,8 +4,16 @@ SET Net_Adapt="Local Area Connection"
 SET IP_Addr=%1
 SET D_Gate=128.3.1.83
 SET Sub_Mask=255.255.0.0
+SET DNS_1=128.3.1.32
+SET DNS_2=128.3.1.33
+
 ECHO "Setting Static IP Information" 
-netsh interface ip SET address %Net_Adapt% static %IP_Addr% %Sub_Mask% %D_Gate% 1 
+netsh interface ip set address %Net_Adapt% static %IP_Addr% %Sub_Mask% %D_Gate% 1 
+ECHO "Setting DNS"
+netsh interface ip set dns %Net_Adapt% static %DNS_1% disabled
+netsh interface ip add dns %Net_Adapt% %DNS_2%
+ECHO "Configuration complete."
+ECHO.
 netsh int ip show config 
 PAUSE
 GOTO end
